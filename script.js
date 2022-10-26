72847e91dc383f4dba1a429b9b38ac20f28bbfe5
farmerbrian <nichols.brian.d@gmail.com>
2022-10-26 15:38:27 -0500

    Added all form inputs, refactored  error logic to function

--- script.js
+++ script.js
@@ -1,21 +1,38 @@
 const form = document.querySelector('form');
 const email = document.getElementById('mail');
-const emailError = document.querySelector('#mail + span.error');
+const country = document.getElementById('country');
+const zip = document.getElementById('zip');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
 
-email.addEventListener('input', (event) => {
-	// Each time the user types something, we check if the
-	// form fields are valid.
+const emailError = document.getElementById('mail-span');
+const countryError = document.getElementById('country-span');
+const zipError = document.getElementById('zip-span');
+const passwordError = document.getElementById('password-span');
+const password2Error = document.getElementById('password2-span');
 
-	if (email.validity.valid) {
-		// In case there is an error message visible, if the field
-		// is valid, we remove the error message.
-		emailError.textContent = ''; // Reset the content of the message
-		emailError.className = 'error'; // Reset the visual state of the message
-	} else {
-		// If there is still an error, show the correct error
-		showError();
-	}
-});
+function inputError(input, error) {
+	input.addEventListener('input', (event) => {
+		// Each time the user types something, we check if the
+		// form fields are valid.
+
+		if (input.validity.valid) {
+			// In case there is an error message visible, if the field
+			// is valid, we remove the error message.
+			error.textContent = ''; // Reset the content of the message
+			error.className = 'error'; // Reset the visual state of the message
+		} else {
+			// If there is still an error, show the correct error
+			showError(input, error);
+		}
+	});
+}
+
+inputError(email, emailError);
+inputError(country, countryError);
+inputError(zip, zipError);
+inputError(password, passwordError);
+inputError(password2, password2Error);
 
 form.addEventListener('submit', (event) => {
 	// if the email field is valid, we let the form submit
@@ -27,22 +44,22 @@ form.addEventListener('submit', (event) => {
 	}
 });
 
-function showError() {
-	if (email.validity.valueMissing) {
+function showError(input, error) {
+	if (input.validity.valueMissing) {
 		// If the field is empty,
 		// display the following error message.
-		emailError.textContent = 'You need to enter an e-mail address.';
-	} else if (email.validity.typeMismatch) {
+		error.textContent = 'You need to enter an e-mail address.';
+	} else if (input.validity.typeMismatch) {
 		// If the field doesn't contain an email address,
 		// display the following error message.
-		emailError.textContent =
+		error.textContent =
 			'Entered value needs to be an e-mail address.';
-	} else if (email.validity.tooShort) {
+	} else if (input.validity.tooShort) {
 		// If the data is too short,
 		// display the following error message.
-		emailError.textContent = `Email should be at least ${email.minLength} characters; you entered ${email.value.length}.`;
+		error.textContent = `Should be at least ${input.minLength} characters; you entered ${input.value.length}.`;
 	}
 
 	// Set the styling appropriately
-	emailError.className = 'error active';
+	error.className = 'error active';
 }
